5eada4e7db9fe9ddb98503ec5c0c77149bb4a243
HLCode <hlcode.contato@gmail.com>
2020-11-20 10:38:57 -0300

    firstCounter

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import {useState} from 'react';
+
+function Home(){
+    return (
+        <div>
+            <h3>Home</h3>
+            <Contador />
+        </div>
+    )
+}
+
+function Contador() {
+    const [contador,setContador] = useState(1);
+
+    function adicionarContador() {
+        setContador(contador + 1);
+    }
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+        </div>
+    )
+}
+
+export default Home
