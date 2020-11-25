a31327ce2e59acfe34288b20035268c859d57b49
HLCode <hlcode.contato@gmail.com>
2020-11-25 17:43:03 -0300

    try to fix header_3

--- pages/index.js
+++ pages/index.js
@@ -85,7 +85,13 @@ const Home_render = (
 
 function Home() {
   return (
-    Header,
+    <div>
+      <Head>
+        <title>This page has a title 🤔</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+    </div>,
     Home_render
   )
 }
