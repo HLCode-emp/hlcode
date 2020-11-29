15873017216712b8073bb932b3aca52d86486502
HLCode <hlcode.contato@gmail.com>
2020-11-29 00:12:06 -0300

    email system sendinble_fix04

--- pages/api/send-mail.js
+++ pages/api/send-mail.js
@@ -30,7 +30,7 @@ export default function handler(req, res) {
         whatsapp: ${data.whatsapp}
         description: ${data.description}`
     }).then(info =>{
-        res.render('../index.js')
+        res.render('/')
     }).catch(error => {
         res.end(res.send(error))
     })
