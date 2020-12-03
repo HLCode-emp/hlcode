5596201e67e16195df79d4a1ca7de8bd46ba3e29
HLCode <hlcode.contato@gmail.com>
2020-12-03 13:49:42 -0300

    icon tab inserted

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <Head>
+        <link rel="shortcut icon" href="http://localhost:3000/public/favicon.ico" />
+    </Head>,
+
+    <Component {...pageProps} />
+  )
 }
 
 export default MyApp
