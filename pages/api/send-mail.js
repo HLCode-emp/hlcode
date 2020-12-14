5e4fede10f9b4064b2f47354197d11fd86928a20
HLCode-emp <hlcode.contato@gmail.com>
2020-12-14 17:26:53 -0300

    sending msh

--- pages/api/send-mail.js
+++ pages/api/send-mail.js
@@ -2,40 +2,37 @@ const nodeMailer = require('nodemailer');
 require('dotenv').config()
 
 const user_mail = {
-    user: process.env.user,
-    pass: process.env.pass
+  user: process.env.user,
+  pass: process.env.pass
 }
 
 const transporter = nodeMailer.createTransport({
-    host: "smtp-relay.sendinblue.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: user_mail.user,
-      pass: user_mail.pass
-    }
-  });
+  host: "smtp-relay.sendinblue.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: user_mail.user,
+    pass: user_mail.pass
+  }
+});
 
-  export default function handler (req, res) {
-    //getting data
-    var data = req.body
-    // sending mail
-    transporter.sendMail({
-        from: user_mail.user,
-        to: user_mail.user,
-        subject: "Entrar em contato",
-        text: `
+export default function handler(req, res) {
+  //getting data
+  var data = req.body
+  // sending mail
+  transporter.sendMail({
+    from: user_mail.user,
+    to: user_mail.user,
+    subject: "Entrar em contato",
+    text: `
         name: ${data.name}
         email: ${data.email}
         phone: ${data.telephone}
         whatsapp: ${data.whatsapp}
         description: ${data.description}`
-    }).then(info =>{
-      res.redirect(301, '/portifolio')
-    }).catch(error => {
-      res.send(error)
-    })
-  }
-
-
-
+  }).then(info => {
+    res.redirect(301, '/portifolio')
+  }).catch(error => {
+    res.send(user_mail)
+  })
+}
